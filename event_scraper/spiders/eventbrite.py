008476a0dd1813fb6397a0acbce78bdb5eaf1c16
syed-niotic <syed.ahmed@niotic.com>
2016-09-14 20:30:48 -0400

    added compatibility for vimbly

--- event_scraper/spiders/eventbrite.py
+++ event_scraper/spiders/eventbrite.py
@@ -6,9 +6,9 @@ from event_scraper.items import EventScraperItem
 
 class EventbriteSpider(Spider):
     name = "eventbrite"
-    allowed_domains = ["eventbrite.ca"]
+    allowed_domains = ["vimbly.com"]
     start_urls = [
-        "https://www.eventbrite.ca/d/canada--toronto/events/"
+        "https://www.vimbly.com/"
     ]
 
     def parse(self, response):
@@ -18,10 +18,10 @@ class EventbriteSpider(Spider):
 
         for site in sites:
             item = EventScraperItem()
-            item['name'] = map(unicode.strip, site.xpath('//div[@class="list-card__title"]/text()').extract())
-            item['date'] = map(unicode.strip, site.xpath('//time[@class="list-card__date"]/text()').extract())
-            item['price'] = map(unicode.strip, site.xpath('//span[@class="list-card__label"]/text()').extract())
-            item['venue'] = map(unicode.strip, site.xpath('//div[@class="list-card__venue"]/text()').extract())
+            item['name'] = map(unicode.strip, site.xpath('//h2/a/text()').extract())
+            item['date'] = map(unicode.strip, site.xpath('//a[@class="firstTime"]/text()').extract())
+            item['price'] = map(unicode.strip, site.xpath('//span[@class="currentPrice"]/text()').extract())
+            item['venue'] = map(unicode.strip, site.xpath('//div[@class="description"]/text()').extract())
             items.append(item)
 
         return items
